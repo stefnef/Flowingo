e6b773c1394123604cecd1c2b0509d4eb41e1c40
stefnef <steffen@vorhundert.de>
2024-05-26 19:24:59 +0200

    wip added error handler
    check for errors in handler

--- app/internal/api/http/handler/error_handler.go
+++ app/internal/api/http/handler/error_handler.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/stefnef/Flowingo/m/internal/core/domain"
 	"net/http"
 )
 
@@ -13,8 +15,14 @@ type ErrorHandlerImpl struct {
 }
 
 func (errorHandler *ErrorHandlerImpl) HandleErrors(context *gin.Context) {
-	for range context.Errors {
-		context.AbortWithStatus(http.StatusNotFound)
+	for _, err := range context.Errors {
+		var notFoundError *domain.NotFoundError
+		switch {
+		case errors.As(err.Err, &notFoundError):
+			context.AbortWithStatusJSON(http.StatusNotFound, err.JSON())
+		default:
+			context.AbortWithStatusJSON(http.StatusInternalServerError, map[string]string{"error": "internal server error"})
+		}
 	}
 }
 
--- app/internal/api/http/handler/test/error_handler_test.go
+++ app/internal/api/http/handler/test/error_handler_test.go
@@ -1,6 +1,8 @@
 package handler_test
 
 import (
+	"errors"
+	"github.com/goccy/go-json"
 	"github.com/stefnef/Flowingo/m/internal/api/http/handler"
 	"github.com/stefnef/Flowingo/m/internal/core/domain"
 	"github.com/stretchr/testify/assert"
@@ -26,11 +28,31 @@ func Test_should_do_nothing_if_there_is_no_error(t *testing.T) {
 func Test_should_map_not_found(t *testing.T) {
 	var context, recorder = GetTestGinContext()
 	recorder.Code = http.StatusCreated
-
 	var err = domain.NewNotFoundError("fake", "some-id")
+	var errorDto map[string]string
 	_ = context.Error(err)
 
 	errorHandler.HandleErrors(context)
 
 	assert.Equal(t, http.StatusNotFound, recorder.Code)
+	var errJson = json.Unmarshal(recorder.Body.Bytes(), &errorDto)
+
+	assert.Nil(t, errJson)
+	assert.Equal(t, map[string]string{"error": "could not find resource 'fake' with id 'some-id'"}, errorDto)
+}
+
+func Test_should_unknown_error_to_server_error(t *testing.T) {
+	var context, recorder = GetTestGinContext()
+	recorder.Code = http.StatusCreated
+	var err = errors.New("i am an unknown error")
+	var errorDto map[string]string
+	_ = context.Error(err)
+
+	errorHandler.HandleErrors(context)
+
+	assert.Equal(t, http.StatusInternalServerError, recorder.Code)
+	var errJson = json.Unmarshal(recorder.Body.Bytes(), &errorDto)
+
+	assert.Nil(t, errJson)
+	assert.Equal(t, map[string]string{"error": "internal server error"}, errorDto)
 }
